73181190b7910c41b295f3741d4e06447d97e1f2
Keith Garcia <Keith@Keiths-MacBook.local>
2017-03-31 13:50:58 -0400

    "Going back"i

---
+++ src/Customer.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Keith on 3/31/17.
+ */
+public class Customer {
+    String name;
+    String option;
+
+    public void chooseName() {
+        System.out.println("What is your name?");
+        if ()
+            name = ATM.scanner.nextLine();
+        System.out.println("Hi " + name);
+        if ()
+        }
+    }
+
